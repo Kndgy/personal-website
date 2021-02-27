45692c3d44e2c2093ed73317beec679938e06528
Rafli Ghani <ghanirafli8@gmail.com>
2021-02-27 14:06:56 +0700

    font awesome icon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core';
 import './App.css';
+library.add(fab, fas);
 
 function discord(){
   window.open("https://discord.com/channels/@me")
@@ -17,38 +22,37 @@ function mail(){
 function App() {
   return (
     <div className="App">
-      <div className="vertical">
       
-        <div className="ver st">
+        <div className="vertical">
+        
+          <div className="ver st">
+          </div>
 
+          <div className="ver nd">
+          </div>
+        
         </div>
+        <div className="block">
 
-        <div className="ver nd">
-
-        </div>
-        
-      </div>
-      <div className="block">
-
-        
-          <div className="tab first" style={{cursor: "pointer"}} onClick={discord} >
-            Discord <span className="hover first"> Kan#9105</span>
-          </div>
-        
-          <div className="tab second" style={{cursor: "pointer"}} onClick={github}>
-            Github <span className="hover first">Kndgy</span>
-          </div>
-         
-          <div className="tab third" style={{cursor: "pointer"}} onClick={linkedin}>
-            LinkedIn <span className="hover first">Rafli Ghani</span>
-          </div>
+            <div className="tab first" style={{cursor: "pointer"}} onClick={discord} >
+              <FontAwesomeIcon icon={['fab', 'discord']} style={{marginRight:'10px'}}/>Discord <span className="hover first"> Kan#9105</span>
+            </div>
           
-          <div className="tab fourth" style={{cursor: "pointer"}} onClick={mail}>
-            Email <span className="hover first">ghanirafli8@gmail.com</span>
-          </div>
-         
+            <div className="tab second" style={{cursor: "pointer"}} onClick={github}>
+              <FontAwesomeIcon icon={['fab', 'github-square']} style={{marginRight:'10px'}}/>Github <span className="hover first">Kndgy</span>
+            </div>
+          
+            <div className="tab third" style={{cursor: "pointer"}} onClick={linkedin}>
+              <FontAwesomeIcon icon={['fab', 'linkedin']} style={{marginRight:'10px'}}/>LinkedIn <span className="hover first">Rafli Ghani</span>
+            </div>
+            
+            <div className="tab fourth" style={{cursor: "pointer"}} onClick={mail}>
+              <FontAwesomeIcon icon={['fas', 'envelope-square']} style={{marginRight:'10px'}}/>Email <span className="hover first">ghanirafli8@gmail.com</span>
+            </div>
+          
+        </div>
       </div>
-    </div>
+   
   );
 }
 
